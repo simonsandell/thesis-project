c77ad6491c927eea349ebf06efc9036a0cc17f20
simonsandell <simonsandell@live.se>
2018-01-27 17:51:05 +0100

    fixed some symbolic links and removed hardcoded directory from runsim.py

--- scripts/runsim.py
+++ scripts/runsim.py
@@ -1,4 +1,13 @@
 import subprocess
+import os
+dl = os.path.dirname(os.path.realpath(__file__)).rsplit("/")
+dn = dl[len(dl)-1]
+print(dl)
+print(dn)
+dir_list = os.getcwd().rsplit("/")
+dir_name = dir_list[len(dir_list)-1]
+print(dir_list)
+print(dir_name)
 def temperatureRun():
     Tstart = 1
     Tend = 3
@@ -14,19 +23,19 @@ def temperatureRun():
         for t in range(20):
             T = T + dt;
             for L in Lrange:
-                subprocess.call(["./3DXYMetropolis/a.out"+ " " +str(L)+ " " +str(T)+ " " +str(1)+ " " +str(N_equil) + " "+ str(N_samples)+" >> "+" ./3DXYMetropolis/output/homedata"],shell=True)
+                subprocess.call(["./a.out"+ " " +str(L)+ " " +str(T)+ " " +str(1)+ " " +str(N_equil) + " "+ str(N_samples)+" >> "+" ./output/temperatureRun"],shell=True)
 
 
 def nequilRun(L,T,Nruns,Neqrange):
     N_samples =1;
     for i in range(Nruns):
         for n in Neqrange:
-            subprocess.call(["./3DXYMetropolis/a.out"+" "+str(L)+" "+str(T)+" 0 "+str(n)+" 1 >>  ./3DXYMetropolis/output/homedataT_M0_"+str(L)],shell=True)
-            subprocess.call(["./3DXYMetropolis/a.out"+" "+str(L)+" "+str(T)+" 1 "+str(n)+" 1 >>  ./3DXYMetropolis/output/homedataT_M1_"+str(L)],shell=True)
+            subprocess.call(["./a.out"+" "+str(L)+" "+str(T)+" 0 "+str(n)+" 1 >>  ./output/nequil"+str(L)],shell=True)
+            subprocess.call(["./a.out"+" "+str(L)+" "+str(T)+" 1 "+str(n)+" 1 >>  ./output/nequil"+str(L)],shell=True)
 
 
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-Neqrange = [65536,131072,262144,524288,1048576]
-nequilRun(4,2.2,10,Neqrange)
+#Neqrange = [65536,131072,262144,524288,1048576]
+#nequilRun(4,2.2,10,Neqrange)
 #temperatureRun();
