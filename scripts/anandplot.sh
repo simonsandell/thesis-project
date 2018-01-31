83dd0ceb90945aa304942cfcc78ba9f0747c1129
simonsandell <simonsandell@live.se>
2018-01-31 11:12:56 +0100

    made formatting better

--- scripts/anandplot.sh
+++ scripts/anandplot.sh
@@ -6,45 +6,45 @@ done
 
 #Display a selection of plots from the output of analyzeData.py using xmgrace
 
-enstring=""
+enstring="-param ../scripts/default.par"
 for filename in ./foutput/en/*; do
 	enstring="$enstring -settype xydy $filename"
 done
-xmgrace $enstring -nosafe -noask -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Energy"' &
+xmgrace $enstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Energy"' &
 
-magstring=""
+magstring="-param ../scripts/default.par"
 for filename in ./foutput/mag/*; do
 	magstring="$magstring -settype xydy $filename"
 done
-xmgrace $magstring -nosafe -noask -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Magnetization"' &
+xmgrace $magstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Magnetization"' &
 
-teqstring=""
+teqstring="-param ../scripts/default.par"
 for filename in ./foutput/teq/*; do
 	teqstring="$teqstring -settype xydy $filename"
 done
-xmgrace $teqstring -log x -pexec 'xaxis label "N_equil"' -pexec 'yaxis label "Magnetization"' -nosafe -noask &
+xmgrace $teqstring -log x -pexec 'xaxis label "N_equil"' -pexec 'yaxis label "Magnetization"' -nosafe -noask -legend load &
 
-binstring=""
+binstring="-param ../scripts/default.par"
 for filename in ./foutput/bin/*; do
 	binstring="$binstring -settype xydy $filename"
 done
-xmgrace $binstring -nosafe -noask -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Binder Cumulant"' &
+xmgrace $binstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Binder Cumulant"' &
 
-dbdtstring=""
+dbdtstring="-param ../scripts/default.par"
 for filename in ./foutput/dbdt/*; do
 	dbdtstring="$dbdtstring -settype xydy $filename"
 done
-xmgrace $dbdtstring -nosafe -noask -pexec 'xaxis label "Temp"' -pexec 'yaxis label "dB/dT"' &
+xmgrace $dbdtstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "dB/dT"' &
 
-xistring=""
+xistring="-param ../scripts/default.par"
 for filename in ./foutput/xi/*; do
 	xistring="$xistring -settype xydy $filename"
 done
-xmgrace $xistring -nosafe -noask -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Susceptibility"' &
+xmgrace $xistring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Susceptibility"' &
 
-rsstring=""
+rsstring="-param ../scripts/default.par"
 for filename in ./foutput/rs/*; do
 	rsstring="$rsstring -settype xydy $filename"
 done
-xmgrace $rsstring -nosafe -noask -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Superfluid density"' &
+xmgrace $rsstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Superfluid density"' &
 
