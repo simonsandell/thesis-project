5a69ce7676679ede5b1855c1e458467162e3e718
simonsandell <simonsandell@live.se>
2018-02-05 14:21:53 +0100

    multithreading using threadpool now works

--- modular/spider.cpp
+++ modular/spider.cpp
@@ -6,38 +6,33 @@
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
 #include "threadpool/boost/threadpool.hpp"
+#include <ctime>
 
 #include "wolffRun.h"
 #include "wolffHistRun.h"
 
 using namespace std;
 using namespace boost::threadpool;
-class makeRunFuncs {
-	bool hist;
-	double L, N_equil, N_samp;
-	bool cold;
-	double T;
-	double *Temps;
-	int N_temps;
-	public:
-	  void setParams(bool,double,double,double,bool,double,double*,int);
-	  void wolff();
-	  void histWolff();
-};
-void makeRunFuncs::setParams(bool h,double l,double neq,double nsam,bool c,double t,double* tmps,int ntmps){
-	L=l; N_equil=neq; N_samp=nsam;
-	cold=c;
-	T = t;
-	Temps= tmps;
-	N_temps = ntmps;
-}
-void makeRunFuncs::wolff(){
-	wolffRun(L,N_equil,N_samp,cold,T);
-}
-void makeRunFuncs::histWolff(){
-	wolffHistRun(L,N_equil,N_samp,cold,Temps,N_temps);
+//
+//public variables 
+//Parameters
+//
+double L;
+double N_equil;
+double N_sampl;
+bool cold;
+int index_temp;
+double *Temps;
+int N_temps;
+//
+//
+void wr(){
+	wolffRun(L,N_equil,N_sampl,cold,Temps[index_temp]);
 }
 
+void whr(){
+	wolffHistRun(L,N_equil,N_sampl,cold,Temps,N_temps);
+}
 
 double * getTrange(double start, double end, int N){
 	double dt = (end-start)/((double)N-1.0);
@@ -48,26 +43,53 @@ double * getTrange(double start, double end, int N){
 	return T;
 }
 
-//main
-int main(int argc, char* argv[]){
-
-	//
-	cout.precision(17);
-	makeRunFuncs mrf;
+void doThreadPool(){
 	unsigned concurentThreadsSupported = std::thread::hardware_concurrency();
+	cout << concurentThreadsSupported << endl;
 	pool tp(concurentThreadsSupported);
-
-	//PARAMS
-	int N_runs = 10;
-	int N_temps= 10;
-	double* Temps = getTrange(2.2,2.22,N_temps);
-	double L = 8;
-
+	int N_runs = 1;
 	for (int i = 0; i <N_runs; ++i){
 		for (int j = 0; j< N_temps;++j){
-			mrf.setParams(false,L,2000,2000,true,Temps[0],Temps,N_temps);
-			boost::function<void()> asdf(boost::bind(&makeRunFuncs::wolff, mrf));
-			tp.schedule(&asdf);
+			index_temp = j;
+			tp.schedule(&wr);
 		}
 	}
+}
+void doThreadPool2(){
+	pool tp(1);
+	int N_runs = 1;
+	for (int i = 0; i <N_runs; ++i){
+		for (int j = 0; j< N_temps;++j){
+			index_temp = j;
+			tp.schedule(&wr);
+		}
+	}
+}
+
+//main
+int main(int argc, char* argv[]){
+	struct timespec start,middle, finish;
+	double elapsed, elapsed2;
+	clock_gettime(CLOCK_MONOTONIC,&start);
+	//initialize parameters
+	L = 8.0;
+	N_equil = 2000.0;
+	N_sampl = 3000.0;
+	cold =false;
+	index_temp = 0;
+	N_temps = 8;
+	Temps = getTrange(2.2,2.22,N_temps);
+	//
+	cout.precision(17);
+
+	doThreadPool();
+	clock_gettime(CLOCK_MONOTONIC,&middle);
+	elapsed = (middle.tv_sec - start.tv_sec);
+	elapsed += (middle.tv_nsec - start.tv_nsec)/1000000000.0;
+	cout << elapsed << endl;
+	doThreadPool2();
+	clock_gettime(CLOCK_MONOTONIC,&finish);
+	elapsed2 = (finish.tv_sec - middle.tv_sec);
+	elapsed2 += (finish.tv_nsec - middle.tv_nsec)/1000000000.0;
+	cout << elapsed2 << endl;
 }
