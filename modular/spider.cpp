09ddbcb2e462d50f507a04f0d17d9dbf3bedc3aa
simonsandell <simonsandell@live.se>
2018-02-05 11:46:14 +0100

    Began implementing threadpool

--- modular/spider.cpp
+++ modular/spider.cpp
@@ -3,56 +3,71 @@
 #include <utility>
 #include <tuple>
 #include <thread>
-#include <vector>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include "threadpool/boost/threadpool.hpp"
 
 #include "wolffRun.h"
 #include "wolffHistRun.h"
 
 using namespace std;
+using namespace boost::threadpool;
+class makeRunFuncs {
+	bool hist;
+	double L, N_equil, N_samp;
+	bool cold;
+	double T;
+	double *Temps;
+	int N_temps;
+	public:
+	  void setParams(bool,double,double,double,bool,double,double*,int);
+	  void wolff();
+	  void histWolff();
+};
+void makeRunFuncs::setParams(bool h,double l,double neq,double nsam,bool c,double t,double* tmps,int ntmps){
+	L=l; N_equil=neq; N_samp=nsam;
+	cold=c;
+	T = t;
+	Temps= tmps;
+	N_temps = ntmps;
+}
+void makeRunFuncs::wolff(){
+	wolffRun(L,N_equil,N_samp,cold,T);
+}
+void makeRunFuncs::histWolff(){
+	wolffHistRun(L,N_equil,N_samp,cold,Temps,N_temps);
+}
 
+
+double * getTrange(double start, double end, int N){
+	double dt = (end-start)/((double)N-1.0);
+	double *T = new double[N];
+	for (int i =0; i< N; ++i){
+		T[i] = start + (double)i*dt;
+	}
+	return T;
+}
 
 //main
 int main(int argc, char* argv[]){
 
-	int Num_Threads = thread::hardware_concurrency();
-	vector<thread> Pool;
-	//set precision of cout
+	//
 	cout.precision(17);
+	makeRunFuncs mrf;
+	unsigned concurentThreadsSupported = std::thread::hardware_concurrency();
+	pool tp(concurentThreadsSupported);
 
+	//PARAMS
+	int N_runs = 10;
+	int N_temps= 10;
+	double* Temps = getTrange(2.2,2.22,N_temps);
+	double L = 8;
 
-	// set up run paramters
-	//
-	//wolffHistRun(double L,  double N_equil_sweeps,  double N_samples,  bool cold,  double *Temperatures, int N_temps );
-	//wolffRun(    double L,  double N_equil_sweeps,  double N_samples,  bool cold,  double Temperature );
-	//
-	double L = 4.0;
-	double N_equil_sweeps = 1000.0;
-	double N_samples = 2000.0;
-	bool coldStart = true;
-	int N_runs = 100;
-
-	double T = 	2.10;
-	double dT = 	0.01;
-	int N_temps = 10;
-	double Temps[N_temps];
-	for (int k = 0; k < N_temps; ++k){
-		Temps[k] = T;
-		T = T + dT;
-	}
-
-	for ( int i = 0; i< N_runs; ++i){
-		//regular run
-		/*
-
-		   for (int j = 0;j< N_temps; ++j){
-
-		   wolffRun(L,N_equil_sweeps,N_samples,coldStart,Temps[j]);
-
-		   }
-		   */
-
-		//hist run
-		wolffHistRun(L,N_equil_sweeps,N_samples,coldStart,Temps,N_temps);
-		wolffHistRun(8.0,N_equil_sweeps,N_samples,coldStart,Temps,N_temps);
+	for (int i = 0; i <N_runs; ++i){
+		for (int j = 0; j< N_temps;++j){
+			mrf.setParams(false,L,2000,2000,true,Temps[0],Temps,N_temps);
+			boost::function<void()> asdf(boost::bind(&makeRunFuncs::wolff, mrf));
+			tp.schedule(&asdf);
+		}
 	}
 }
