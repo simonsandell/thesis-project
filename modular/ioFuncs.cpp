e3c312b227732bbf6a6c5a00ad4acf7ded5f54f3
simonsandell <simonsandell@live.se>
2018-02-07 15:48:05 +0100

    renamed some files

--- modular/ioFuncs.cpp
+++ modular/ioFuncs.cpp
@@ -1,5 +1,8 @@
+#include <limits>
+#include <ctime>
 #include <iostream>
-#include "printOutput.h"
+#include <fstream>
+#include "ioFuncs.h"
 
 void printOutput(long double L,long double T, long double E, long double M, long double bin, long double dbdt,long double xi,long double rs, long double N_eq_sweeps, int N_eq_clusts){
 
@@ -16,3 +19,25 @@ void printOutput(long double L,long double T, long double E, long double M, long
 	std::cout << std::fixed << std::endl;
 }
 
+long double getMaxE(){
+
+	std::ifstream file("maxE.txt");
+	long double maxE;
+	file >> std::fixed >> maxE;
+	return maxE;
+}
+void setMaxE(long double newE){
+
+	time_t  t = time(0);
+	struct tm * now = localtime(& t);
+
+	char buffer [80];
+	strftime (buffer,80,"%Y-%m-%d.%H:%M:%S",now);
+
+	std::ofstream file;
+	file.open(buffer);
+
+	typedef std::numeric_limits<long double> dbl;
+	file.precision(dbl::max_digits10 +2);
+	file << std::fixed << newE;
+}
