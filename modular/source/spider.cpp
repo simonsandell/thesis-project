1c0968cb45fa57a5fedb0487311aef594912f936
simonsandell <simonsandell@live.se>
2018-02-13 17:09:17 +0100

    asdf

--- modular/source/spider.cpp
+++ modular/source/spider.cpp
@@ -25,21 +25,39 @@ long double * getTrange(long double start, long double end, int N){
 //main
 int main(int argc, char* argv[]){
 	// input args :: 'wolff'/'hist' L startT endT Tnum Neq Nsamp Cold
+	long double runType ;
+	long double L;
+	long double startT;
+	long double endT;
+	long double Ntemps;
+	long double Neq;
+	long double Nsamp;
+	long double icold;
 	// 
 	//set precision of cout
 	if (argc != 10){
-		cout << "wrong number of arguments" << endl;
-		exit(-1);
+		//cout << "wrong number of arguments" << endl;
+		//exit(-1);
+		runType = 1.0L;
+		L= 4.0L;
+		startT=2.201L;
+		endT=2.203L;
+		Ntemps=30.0L;
+		Neq=6000.0L;
+		Nsamp=20000.0L;
+		icold=1.0L;
 	}
+	else{
 
-	long double runType = stod(argv[1]);
-	long double L= stod(argv[2]);
-	long double startT= stod(argv[3]);
-	long double endT= stod(argv[4]);
-	long double Ntemps= stod(argv[5]);
-	long double Neq= stod(argv[6]);
-	long double Nsamp= stod(argv[7]);
-	long double icold= stod(argv[8]);
+		runType = stod(argv[1]);
+		L= stod(argv[2]);
+		startT= stod(argv[3]);
+		endT= stod(argv[4]);
+		Ntemps= stod(argv[5]);
+		Neq= stod(argv[6]);
+		Nsamp= stod(argv[7]);
+		icold= stod(argv[8]);
+	}
 	//set cold start bool
 	bool cold = false;
 	if (icold == 1.0L){
@@ -55,7 +73,27 @@ int main(int argc, char* argv[]){
 		Trange = getTrange(startT,endT,int(Ntemps));
 	}
 	long double runTemp = Trange[(int)(Ntemps/2)];
+	//warmup for 6000 sweeps
+	warmup(L,Neq,cold,runTemp,true);
+	long double Neqsw;
+	long double Neqcl;
+	long double *** lattice;
+	for (int i = 0; i< 5; ++i){
+		lattice = getLattice(L,Neqsw,Neqcl);
+		wolffHistRun(L,lattice,Neqsw,Neqcl,Nsamp,cold,Trange,Ntemps,runTemp);
+	}
+	L = 8.0L;
+	warmup(L,Neq,cold,runTemp,true);
+	for (int i = 0; i< 10000; ++i){
+		lattice = getLattice(L,Neqsw,Neqcl);
+		wolffHistRun(L,lattice,Neqsw,Neqcl,Nsamp,cold,Trange,Ntemps,runTemp);
+	}
+
+
+
+
 	// determine if saving warmup, loading warmup, or warming up and running.
+	/*
 	std::string argwarmup= argv[9];
 	if (argwarmup == "save") {
 		warmup(L,Neq,cold,runTemp,true);
@@ -65,7 +103,8 @@ int main(int argc, char* argv[]){
 		long double N_equil_clusters;
 		long double ***lattice = getLattice(L,N_equil_sweeps,N_equil_clusters);
 		if (runType == 1.0L){
-		wolffHistRun(L,lattice,N_equil_sweeps,N_equil_clusters,Nsamp,cold,Trange,Ntemps,runTemp);
+			wolffHistRun(L,lattice,N_equil_sweeps,N_equil_clusters,Nsamp,cold,Trange,Ntemps,runTemp);
 		}
 	}
+	*/
 }
--- scripts/newteqana.py
+++ scripts/newteqana.py
@@ -4,9 +4,9 @@ import numpy as np
 import math
 
 #funcion for writing data
-def writeToFiles(Mag,M_teq,N,Neq):
+def writeToFiles(L,Mag,M_teq,N,Neq):
         meanNeq = np.mean(Neq);
-        meanM = np.mean(Mag);
+        meanM = pow(L,-0.518)*np.mean(Mag);
         sqrtN = N.sqrt();
         deltaM = np.std(Mag)/sqrtN;
         if math.isnan(deltaM):
@@ -42,7 +42,7 @@ Mag=[];
 N = decimal.Decimal('0.0');
 
 #open files for writing
-strFn = "./foutput/teq/" + str(int(cold)) + "_" + str(int(L)) + "_" + fName + ".dat"
+strFn = "./foutput/teqmod/" + str(int(cold)) + "_" + str(int(L)) + "_" + fName + ".dat"
 M_teq = open(strFn,"w")
 
 TOL = decimal.Decimal('4') 
@@ -50,7 +50,7 @@ relTOL = decimal.Decimal('0.5')
 for i in range(mat.shape[0]):
     #if new value of L or cold, make new outputfile
     if (abs(mat[i,12] - cold) > decimal.Decimal('0.1')):
-        writeToFiles(Mag,M_teq,N,Neq)
+        writeToFiles(L,Mag,M_teq,N,Neq)
         N = decimal.Decimal('0');
         L=decimal.Decimal(mat[i,0])
         Neq_sav = decimal.Decimal(mat[i,8])
@@ -58,7 +58,7 @@ for i in range(mat.shape[0]):
         strFn = "./foutput/teq/" + str(int(cold)) + "_" + str(int(L)) + "_" + fName + ".dat"
         M_teq = open(strFn,"w")
     elif(TOL < abs(mat[i,0] - L)):
-        writeToFiles(Mag,M_teq,N,Neq)
+        writeToFiles(L,Mag,M_teq,N,Neq)
         N = decimal.Decimal('0');
         L = decimal.Decimal(mat[i,0])
         Neq_sav = decimal.Decimal(mat[i,8])
@@ -66,11 +66,11 @@ for i in range(mat.shape[0]):
         M_teq = open(strFn,"w")
         #if new value of N_eq, take mean and write to file
     elif (relTOL < (decimal.Decimal('1.0')/(Neq_sav))*abs(mat[i,8] - Neq_sav)):
-        writeToFiles(Mag,M_teq,N,Neq)
+        writeToFiles(L,Mag,M_teq,N,Neq)
         N = decimal.Decimal('0');
         Neq_sav = decimal.Decimal(mat[i,8])
     Mag.append(mat[i,3])
     Neq.append(mat[i,8])
     Neq_sav = np.mean(Neq)
     N = N + decimal.Decimal('1.0')
-writeToFiles(Mag,M_teq,N,Neq)
+writeToFiles(L,Mag,M_teq,N,Neq)
