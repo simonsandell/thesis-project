20d5f1d22cc5bc658a9fb82189c649e78474b0e4
simonsandell <simonsandell@live.se>
2018-02-15 16:36:52 +0100

    minor changes

--- scripts/emptyFoutput.sh
+++ scripts/emptyFoutput.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Delete all files in ./foutput/?"
+echo "Delete files matching *$1*  in ./foutput/*/?"
 read answer
 if [ $answer != "Y" ]; then
 	echo "answer 'Y' if you want to delete"
 	exit 1;
 fi
 for dir in ./foutput/*/; do
-	for file in $dir/*; do
+	for file in $dir/*$1*; do
 		rm $file
 	done
 done
--- scripts/rplots.sh
+++ scripts/rplots.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 #Display a selection of plots from the output of analyzeData.py using xmgrace
 
-enstring="-param ../scripts/default.par"
-for filename in ./foutput/en/*; do
-	enstring="$enstring -settype xydy $filename"
-done
-xmgrace $enstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Energy"' &
-
-magstring="-param ../scripts/default.par"
-for filename in ./foutput/mag/*; do
-	magstring="$magstring -settype xydy $filename"
-done
-xmgrace $magstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Magnetization"' &
-
-
+#Binder paramter
 binstring="-param ../scripts/default.par"
 for filename in ./foutput/bin/*; do
 	binstring="$binstring -settype xydy $filename"
 done
 xmgrace $binstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Binder Cumulant"' &
 
-dbdtstring="-param ../scripts/default.par"
-for filename in ./foutput/dbdt/*; do
-	dbdtstring="$dbdtstring -settype xydy $filename"
-done
-xmgrace $dbdtstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "dB/dT"' &
-
-xistring="-param ../scripts/default.par"
-for filename in ./foutput/xi/*; do
-	xistring="$xistring -settype xydy $filename"
-done
-xmgrace $xistring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Susceptibility"' &
-
+#Superfluid density
 rsstring="-param ../scripts/default.par"
 for filename in ./foutput/rs/*; do
 	rsstring="$rsstring -settype xydy $filename"
 done
 xmgrace $rsstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Superfluid density"' &
 
+#Only plot rest if requested
+if [ ! $# -eq 0 ]
+then
+	#Energy per spin
+	enstring="-param ../scripts/default.par"
+	for filename in ./foutput/en/*; do
+		enstring="$enstring -settype xydy $filename"
+	done
+	xmgrace $enstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Energy"' &
+
+	#Magnetization per spin
+	magstring="-param ../scripts/default.par"
+	for filename in ./foutput/mag/*; do
+		magstring="$magstring -settype xydy $filename"
+	done
+	xmgrace $magstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Magnetization"' &
+
+
+	#dB/dT
+	dbdtstring="-param ../scripts/default.par"
+	for filename in ./foutput/dbdt/*; do
+		dbdtstring="$dbdtstring -settype xydy $filename"
+	done
+	xmgrace $dbdtstring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "dB/dT"' &
+	#Susceptibility
+	xistring="-param ../scripts/default.par"
+	for filename in ./foutput/xi/*; do
+		xistring="$xistring -settype xydy $filename"
+	done
+	xmgrace $xistring -nosafe -noask -legend load -pexec 'xaxis label "Temp"' -pexec 'yaxis label "Susceptibility"' &
+fi
 
