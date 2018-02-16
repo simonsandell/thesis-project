72d89849aa130f31b2ba07d3f65df025823eb9e4
simonsandell <simonsandell@live.se>
2018-02-16 13:48:03 +0100

    started preparing analyzeData for new output format

--- scripts/analyzeData.py
+++ scripts/analyzeData.py
@@ -3,20 +3,10 @@ import numpy as np
 import math
 
 #funcion for writing data
-def writeToFiles(En,Mag,Bin,Dbdt,Xi,Rs,EF,MF,BF,DF,XF,RF,N,T,Neq):
-        meanE = np.mean(En);
-        meanM = np.mean(Mag);
-        meanB = np.mean(Bin);
-        meanD = np.mean(Dbdt);
-        meanX = np.mean(Xi);
-        meanR = np.mean(Rs);
-        sqrtN = pow(N,0.5);
-        deltaE = np.std(En)/sqrtN;
-        deltaM = np.std(Mag)/sqrtN;
-        deltaB = np.std(Bin)/sqrtN;
-        deltaD = np.std(Dbdt)/sqrtN;
-        deltaX = np.std(Xi)/sqrtN;
-        deltaR = np.std(Rs)/sqrtN;
+def writeToFile(X,Y,FILE):
+        meanY = np.mean(Y);
+        sqrtN = pow(len(Y),0.5);
+        deltaY = np.std(m)/sqrtN;
         if math.isnan(deltaE):
             deltaE = float('0');
         if math.isnan(deltaM):
@@ -45,9 +35,12 @@ def writeToFiles(En,Mag,Bin,Dbdt,Xi,Rs,EF,MF,BF,DF,XF,RF,N,T,Neq):
 #read raw data from file in ./output
 #################################
 # Format::
-# 0      1      2      3      4      5      6      7      8      9      10      11      12      
-# L      T      E      M      B      dBdT   xi     rs     eqsw   eqcl   smsw    smcl    cold
-
+# 0      1      2      3      4      5      6            
+# l      t      neqsw  neqcl  nsmsw  nsmcl  cold
+# 7      8      9      10     11     12     13                
+# E      E2     M      M2     M4     M2E    M4E
+# 14     15     16     17     18     19     20     21                
+# SX     SY     SZ     bin    dBdT   xi     rs     expFac
 arguments = sys.argv
 fName = arguments[1]
 data0 = open("./output/" + fName,"r")
@@ -60,21 +53,18 @@ for ln in data0:
 
 #sort by L, T, N_equil in that order    
 mat = np.array(vals)
-ind = np.lexsort((mat[:,12],mat[:,11],mat[:,10],mat[:,9],mat[:,7],mat[:,6],mat[:,5],mat[:,4],mat[:,3],mat[:,2],mat[:,8],mat[:,1],mat[:,0]))
+
+ind = np.lexsort((mat[:,21],mat[:,20],mat[:,19],mat[:,18],mat[:,17],mat[:,16],mat[:,15],mat[:,14],mat[:,13],mat[:,12],mat[:,11],mat[:,10],mat[:,9],mat[:,8],mat[:,7],mat[:,5],mat[:,4],mat[:,3],mat[:,2],mat[:,6],mat[:,1],mat[:,0]));
 
 mat = mat[ind]
 
 L=mat[0,0];
 T=mat[0,1];
-Neq = mat[0,7];
-En=[];
-Mag=[];
-Bin=[];
-Dbdt=[];
-Xi=[];
-Rs=[];
-NeqSw=[];
-NeqCl=[];
+Neq_sw = mat[0,2];
+
+
+sumlist = mat[0,:];
+
 N = float('0.0');
 
 #open files for writing
@@ -83,15 +73,19 @@ N = float('0.0');
 EF = open("./foutput/en/"+str(int(L))+"_"+fName+".dat","w")
 MF = open("./foutput/mag/"+str(int(L))+"_"+fName+".dat","w")
 BF = open("./foutput/bin/"+str(int(L))+"_"+fName+".dat","w")
+newBF = open("./foutput/bin/"+str(int(L))+"_"+fName+"new.dat","w")
 DF = open("./foutput/dbdt/"+str(int(L))+"_"+fName+".dat","w")
+newDF = open("./foutput/dbdt/"+str(int(L))+"_"+fName+"new.dat","w")
 XF = open("./foutput/xi/"+str(int(L))+"_"+fName+".dat","w")
+newXF = open("./foutput/xi/"+str(int(L))+"_"+fName+"new.dat","w")
 RF = open("./foutput/rs/"+str(int(L))+"_"+fName+".dat","w")
+newRF = open("./foutput/rs/"+str(int(L))+"_"+fName+"new.dat","w")
 
-TOL = float('0.0000000000001');
+TOL = float('0.00000000001');
 for i in range(mat.shape[0]):
     #if new value of L, make new outputfile
     if(TOL < abs(mat[i,0] - L)):
-        writeToFiles(En,Mag,Bin,Dbdt,Xi,Rs,EF,MF,BF,DF,XF,RF,N,T,Neq)
+        writeToFile(En,Mag,Bin,Dbdt,Xi,Rs,EF,MF,BF,DF,XF,RF,N,T,Neq)
         N=0;
         L = float(mat[i,0])
         T = float(mat[i,1])
