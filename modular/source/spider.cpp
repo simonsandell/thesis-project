b5bfef7cf16dd2a727697454cb7af335c90535c8
simonsandell <simonsandell@live.se>
2018-02-23 11:37:20 +0100

    now uses multithreadingjJwq :wq

--- modular/source/spider.cpp
+++ modular/source/spider.cpp
@@ -3,7 +3,10 @@
 #include <utility>
 #include <tuple>
 #include <limits>
+#include <vector>
+#include <chrono>
 
+#include "ThreadPool.h"
 #include "ioFuncs.h"
 #include "calcQuants.h"
 #include "latticeOps.h"
@@ -52,9 +55,8 @@ long double * getTrange(long double start, long double end, int N){
 	}
 	return T;
 }
+void wolffHistJob(){
 
-//main
-int main(){
 	//
 	//Set Run Parameters
 	//
@@ -89,10 +91,56 @@ int main(){
 	//
 	//End of parameters
 	//
-	
-	
 	Lattice lat(L,cold);
 	Cluster clust(L);
+	RandStruct rand;
+	//initial warmup
+	long double beta = 1.0L/runTemp;
+	warmup(lat,clust,beta,rand,(Neq-Nbetw));
+	//sample
+	for (int i=0; i< Nruns; ++i){	
+		warmup(lat,clust,beta,rand,Nbetw);
+		wolffHistRun(lat,Nsamp,Trange,Ntemps,runTemp);
+	}
+
+}
+void metroJob(){
+
+	//
+	//Set Run Parameters
+	//
+	//
+
+	//temperatures
+	long double	startT=			2.20150L;
+	long double	endT=			2.20350L;
+	int 		Ntemps=			21.0L;
+	//generate temperature range
+	long double* Trange;
+	if (abs(Ntemps - 1) < 0.1) {
+		Trange = new long double[1];
+		Trange[0] = startT;
+	}
+	else {
+		Trange = getTrange(startT,endT,int(Ntemps));
+	}
+	long double runTemp = 2.20200000000000L;
+	//system size
+	long double 	L =			4.0L;
+	//initial equilibration sweeps
+	int 		Neq=			20000;
+	//initial configuration
+	bool 		cold=			true;
+	//sample sweeps per run
+	long double	Nsamp=			1000.0L;
+	//eq sweeps between runs
+	int 		Nbetw=			100;
+	//number of total runs
+	int 		Nruns=			1000;
+	//
+	//End of parameters
+	//
+	Lattice lat(L,cold);
 	RandStruct rand;
 	//initial warmup
 	long double beta = 1.0L/runTemp;
@@ -103,5 +151,19 @@ int main(){
 		metroRun(lat,Nsamp,runTemp);
 	}
 
+}
+
+//main
+int main(){
+	ThreadPool pool(24);
+	std::vector< std::future<void> > results;
+
+	for(int i = 0; i < 8; ++i) {
+		results.emplace_back(
+				pool.enqueue([i] {
+					wolffHistJob();
+					})
+				);
+	}
 }
 
