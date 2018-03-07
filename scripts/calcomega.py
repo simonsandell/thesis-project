fe236e3aa294a6c6102061a0c5922b5af84754e6
simonsandell <simonsandell@live.se>
2018-03-07 11:48:52 +0100

    calculation of omega can now handle system sizes up to 128

--- scripts/calcomega.py
+++ scripts/calcomega.py
@@ -17,11 +17,17 @@ for ln in dat:
 
 T = vals[0][4];
 fnfstr = "{:8.8f}"
-ofile = open("./foutput/omega/omega.dat","a")
+o1file = open("./foutput/omega/omega1.dat","a")
+o2file = open("./foutput/omega/omega2.dat","a")
+o3file = open("./foutput/omega/omega3.dat","a")
+o4file = open("./foutput/omega/omega4.dat","a")
+flist = [o1file,o2file,o3file,o4file];
 fstr= "{:30.30f}";
-bdiff = vals[1][1] - vals[0][1];
-bdiff2 = vals[2][1] - vals[1][1];
-bdiv = bdiff2/bdiff
-omega = -math.log(bdiv)/math.log(2)
-ofile.write(fstr.format(T) + "    " +fstr.format(omega) + "    " +fstr.format(0.0) + "    " + fstr.format(vals[0][3]) + "    \n")
+for x in range((len(vals) -2 )):
+
+    bdiff = vals[x+1][1] - vals[x+0][1];
+    bdiff2 = vals[x+2][1] - vals[x+1][1];
+    bdiv = bdiff2/bdiff
+    omega = -math.log(bdiv)/math.log(2)
+    flist[x].write(fstr.format(T) + "    " +fstr.format(omega) + "    " +fstr.format(0.0) + "    " + fstr.format(vals[0][3]) + "    \n")
 
--- scripts/calcomega.sh
+++ scripts/calcomega.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm ./foutput/omega/*
 for filename in ./foutput/L_bin/*; do
 	python3 ../scripts/calcomega.py $filename
 done
