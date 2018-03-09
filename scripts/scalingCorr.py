fixed path in omegaplot, wrote big part of scaling corr, need to do jackknife

scripts/scalingCorr.py
@@ -3,35 +3,44 @@ import numpy as np
 import math
 np.set_printoptions(threshold=np.nan)
 
-def openFiles(FileList,N,fName):
-    o1file = open("./foutput/omega/omega1.dat","a")
-    o2file = open("./foutput/omega/omega2.dat","a")
-    o3file = open("./foutput/omega/omega3.dat","a")
-    o4file = open("./foutput/omega/omega4.dat","a")
-    r1file = open("./foutput/rs_corr/rscorr1.dat","a")
-    r2file = open("./foutput/rs_corr/rscorr2.dat","a")
-    r3file = open("./foutput/rs_corr/rscorr3.dat","a")
-    r4file = open("./foutput/rs_corr/rscorr4.dat","a")
-    FileList= [o1file,o2file,o3file,o4file,r1file,r2file,r3file,r4file];
-def closeFiles(FileList):
-    for f in FileList:
-        f.close();
+def openFiles():
+    o1file = open("./foutput/3DXY/omega/omega1.dat","w")
+    o2file = open("./foutput/3DXY/omega/omega2.dat","w")
+    o3file = open("./foutput/3DXY/omega/omega3.dat","w")
+    o4file = open("./foutput/3DXY/omega/omega4.dat","w")
+    r1file = open("./foutput/3DXY/rs_corr/rscorr1.dat","w")
+    r2file = open("./foutput/3DXY/rs_corr/rscorr2.dat","w")
+    r3file = open("./foutput/3DXY/rs_corr/rscorr3.dat","w")
+    r4file = open("./foutput/3DXY/rs_corr/rscorr4.dat","w")
+    return [o1file,o2file,o3file,o4file,r1file,r2file,r3file,r4file];
+def calcCorrections(avgs):
+    bdiff = avgs[1][2] - avgs[0][2];
+    bdiff2 = avgs[2][2] - avgs[1][2];
+    bdiv = bdiff2/bdiff
+    omega = -math.log(bdiv)/math.log(2)
 
-def jackknife(flist):
-    jacklen = float(len(flist))-1.0;
-    listsum = sum(flist);
-    jacklist = [];
-    
-    for i in range(len(flist)):
-        jacklist.append(listsum);
-        jacklist[i] = jacklist[i] - flist[i];
-        jacklist[i] = jacklist[i]/(jacklen);
-    jackavg = np.mean(jacklist);
-    sigmaf = pow(jacklen,0.5)*(np.std(jacklist));
-    return sigmaf;
-    
+    rdiff = avgs[1][1] - avgs[0][1];
+    rdiff2 = avgs[2][1] - avgs[1][1];
+    rdiv = rdiff2/rdiff;
+    #rscorr = -math.log(rdiv)/math.log(2);
+    return [1.0,omega]
 
-def calcAvg(mat,i,istart,FileList):
+def calcAvgs(mat):
+    L = mat[0,0];
+    T = mat[0,1];
+    M2 = np.mean(mat[:,10]);
+    M4 = np.mean(mat[:,11]);
+    Exp = np.mean(mat[:,21]);
+    b = Exp*M4/(M2*M2);
+    E = np.mean(mat[:,7]);
+    S2X = np.mean(mat[:,14]);
+    S2Y = np.mean(mat[:,15]);
+    S2Z = np.mean(mat[:,16]);
+    rs = -L*E - (L*L*L*L/T)*(S2X + S2Y + S2Z);
+    rs = rs/(3.0*Exp);
+    return [L,rs,b]
+
+def analyze(mat,i,istart,FileList):
 # Format::
 # 0      1      2      3      4      5      6            
 # L      T      neqsw  neqcl  nsmsw  nsmcl  cold
@@ -41,29 +50,50 @@ def calcAvg(mat,i,istart,FileList):
 #
 # 14     15     16     17     18     19     20     21                
 # S2X    S2Y    S2Z    bin    dBdT   xi     rs     expFac
+    iend = i -1;
+    submat = mat[istart:iend,:];
+    T = submat[0,1];
+    submatL = submat[:,0];
+    L_list = np.unique(submatL);
+    N_L = L_list.shape[0];
+    j = 0;
+    avgs = []
+    for l in L_list:
+        #find start and end index for each L
+        foundstart = False;
+        foundend = False;
+        lstart = 0;
+        lend = 0;
+        for j in range(submat.shape[0]):
+            if (submat[j,0] == l) and (not foundstart):
+                lstart = j;
+                foundstart= True
+            if (submat[j,0] != l) and foundstart and (not foundend):
+                lend = j;
+                foundend= True;
+        if (not foundend):
+            lend = j;
+        subL = submat[lstart:lend,:];
+        print(lstart);
+        print(lend);
 
-    T = mat[istart,1];
-    startL = mat[istart,0];
-    N_L = 1;
-    avgB = []
-    starti = istart;
-    endi = 0;
-    for x in range(i-istart):
-        if (mat[x,0] != startL):
-            endi = x-1;
-            N_L = N_L +1;
-            startL = mat[x,0];
-            avgExp= np.mean(mat[starti:endi,21]);
-            avgM2= np.mean(mat[starti:endi,10]);
-            avgM4= np.mean(mat[starti:endi,11]);
-            avgB.append((avgExp*avgM4/(avgM2*avgM2)));
-            rawB.append(mat[starti:endi,17]);
-            starti = x;
-    for y in range(N_L -2):
-        
+        avgs.append(calcAvgs(subL));
+
     fstr= "{:30.30f}";
-    for i in range(len(Ylist)):
-        FileList[i].write(fstr.format(L)+"    "+fstr.format(Ylist[i])+"    "+fstr.format(Deltalist[i])+"    "+fstr.format(N)+ "    "+fstr.format(T) + "\n")
+    for k in range(N_L-2):
+        corrs = calcCorrections(avgs[k:k+3]);
+        print(corrs)
+        #FileList[k].write(fstr.format(T) + "    " + fstr.format(corrs[1]) + "    " + 
+                #fstr.format(0.0) + " \n"); 
+        #FileList[k+4].write(fstr.format(T) + "    " + fstr.format(corrs[0]) + "    " + 
+                #fstr.format(0.0) + " \n"); 
+        FileList[k].write(repr(T) + "    " + repr(corrs[1]) + "    " + 
+                repr(0.0) + " \n"); 
+        FileList[k+4].write(repr(T) + "    " + repr(corrs[0]) + "    " + 
+                repr(0.0) + " \n"); 
+
+
+
 
 #
 #read raw data from file in ./output
@@ -77,9 +107,8 @@ def calcAvg(mat,i,istart,FileList):
 #
 # 14     15     16     17     18     19     20     21                
 # SX     SY     SZ     bin    dBdT   xi     rs     expFac
-arguments = sys.argv
-fName = arguments[1]
-data0 = open("./output/" + fName,"r")
+fName= sys.argv[1];
+data0 = open("./output/3DXY/" + fName,"r")
 vals = []
 #load data and form array
 for ln in data0:
@@ -93,17 +122,16 @@ mat = np.array(vals)
 ind = np.lexsort((mat[:,21],mat[:,20],mat[:,19],mat[:,18],mat[:,17],mat[:,16],mat[:,15],mat[:,14],mat[:,13],mat[:,12],mat[:,11],mat[:,10],mat[:,9],mat[:,8],mat[:,7],mat[:,5],mat[:,4],mat[:,3],mat[:,2],mat[:,6],mat[:,0],mat[:,1]));
 mat = mat[ind]
 #form averages and print to file
-startL=mat[0,0];
-currL=mat[0,0];
-
+T = mat[0,1];
 
 TOL = 0.000001;
 ifirst = 0;
+filelist = openFiles();
 for i in range(mat.shape[0]):
-    if (mat[i,0] == startL) and (mat[i,0] != currL):
-        calcAvg(mat,i,ifirst);
+    if (mat[i,1] != T):
+        analyze(mat,i,ifirst,filelist);
         ifirst = i;
-    currL = mat[i,0];
+        T = mat[i,1];
         
 #one final write
-calcAvg(mat,i+1,ifirst);
+analyze(mat,i+1,ifirst,filelist);