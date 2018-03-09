c267477ff3120db21398ad574bd270bf3d645a8a
simonsandell <simonsandell@live.se>
2018-03-09 15:37:35 +0100

    fixed some path

--- scripts/anplot3DXY.sh
+++ scripts/anplot3DXY.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # loop over raw data files, do data analysis on them by calling analyzeData.py 
-for filename in ./output/*; do
+for filename in ./output/3DXY/*; do
 	echo "analyzing $filename"
 	python3 ../scripts/analyze3DXY.py $(basename $filename)
 	python3 ../scripts/L_analyze3DXY.py $(basename $filename)
--- scripts/anplotIsing3D.sh
+++ scripts/anplotIsing3D.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # loop over raw data files, do data analysis on them by calling analyzeData.py 
-for filename in ./output/*; do
+for filename in ./output/Ising3D/*; do
 	echo "analyzing $filename"
 	python3 ../scripts/analyzeIsing3D.py $(basename $filename)
 	python3 ../scripts/L_analyzeIsing3D.py $(basename $filename)
