c48c18b441fdf792ec52caf719c42051bf91399a
simonsandell <simonsandell@live.se>
2018-03-13 14:39:13 +0100

    moved some files

--- scripts/emptyFoutput.sh
+++ scripts/emptyFoutput.sh
@@ -6,7 +6,9 @@ if [ $answer != "Y" ]; then
 	exit 1;
 fi
 for dir in ./foutput/*/; do
-	for file in $dir/*$1*; do
-		rm $file
+	for subdir in $dir/*/; do
+		for file in $subdir/*$1*; do
+			rm $file
+		done
 	done
 done
