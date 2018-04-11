8d6ed478952efd8575eae819fafadd71226552b2
simonsandell <simonsandell@live.se>
2018-04-11 10:31:32 +0200

    cleaned up x value

--- scripts/teqPlot.py
+++ scripts/teqPlot.py
@@ -6,23 +6,27 @@ import numpy as np
 def jfunc(mat):
     return [np.mean(mat[:,9])];
 
-def writeLine(mat,openfile):
+def writeLine(mat,openfile,avgN):
     avgM = jfunc(mat)[0];
     delM = jackknife.getJackDelta(mat,jfunc,100)[0];
     fstr= "{:30.30f}";
-    openfile.write(fstr.format(mat[0,4]) + "    " + fstr.format(avgM) + "    " + fstr.format(delM) + "    " + str(mat.shape[0]) + "\n");
+    openfile.write(fstr.format(avgN) + "    " + fstr.format(avgM) + "    " + fstr.format(delM) + "    " + str(mat.shape[0]) + "\n");
 
 #now we have data for only interesting temp, separete N_sweeps and print
 def oneTemp(mat,openfile):
     startI = 0;
-    curr_N= mat[0,4];
-    relTOL = 0.3;
+    high_N=4.0;
+    avgPrev = 0.0;
+    avgCurr = 0.0;
     for i in range(mat.shape[0]):
-        if (abs(mat[i,4]-curr_N)/curr_N > relTOL):
-            writeLine(mat[startI:i],openfile);
-            curr_N = mat[i,4];
+        if (mat[i,4]>high_N):
+            avgCurr = np.mean(mat[startI:i,4]);
+            writeLine(mat[startI:i],openfile,0.5*(avgCurr + avgPrev));
+            avgPrev = avgCurr;
+            high_N= high_N*2;
             startI = i;
-    writeLine(mat[startI:,:],openfile);
+    avgCurr = np.mean(mat[startI:,4]);
+    writeLine(mat[startI:,:],openfile,0.5*(avgCurr + avgPrev));
 
 def printAll(mat,openfile):
     fstr= "{:30.30f}";
