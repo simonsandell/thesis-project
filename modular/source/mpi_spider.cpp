5a00b456a686cc0a3b981d09d0541e93806e118a
Simon Sandell <simonsandell@live.se>
2018-04-13 14:57:54 +0200

    output now seems to work

--- modular/source/mpi_spider.cpp
+++ modular/source/mpi_spider.cpp
@@ -24,14 +24,14 @@ int main(){
         std::string h_pwd = "/home/simon/exjobb/modular/"; 
 	std::string o_pwd = "/home/simsan/exjobb/modular/"; 
 	
-	std::string choice = b_pwd;
+	std::string choice = o_pwd;
 	std::string model = "3DXY/";
 		  
 	std::string mep= choice + "maxE/" + model; 
 	std::string wlp= choice + "warmLattice/" + model; 
 
 	if (world_rank != 0){
-		_3DXY::warmupJob(64.0L,mep,wlp);
+		_3DXY::teqJob(4.0L,true,mep,wlp);
 		std::string endmsg = "finished";	
 		MPI_Send(endmsg.c_str(),endmsg.size(),MPI_CHAR,0,1,MPI_COMM_WORLD);
 	}
@@ -46,7 +46,7 @@ int main(){
 			MPI_Recv(buf,char_amount,MPI_CHAR,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			if (status.MPI_TAG == 1 || status.MPI_TAG == 0){
 				std::cout << "before mpi_rev message" << std::endl;
-				std::cout.write(buf,sizeof(char)*(char_amount+1));
+				std::cout.write(buf,sizeof(char)*(char_amount));
 				std::cout << "after mpi_rev message" << std::endl;
 			}
 			if (status.MPI_TAG == 1){
