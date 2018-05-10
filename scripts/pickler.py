6a6ee3dbeaa9906e9b06947002dd6458b812c95c
simonsandell <simonsandell@live.se>
2018-05-10 15:52:49 +0200

    pickle larger size

--- scripts/pickler.py
+++ scripts/pickler.py
@@ -1,10 +1,22 @@
 import pickle
+import numpy as np
 
 def saveData(data, fName="graph"):
+    n_bytes = data.size * data.dtype.itemsize; 
+    max_bytes = 2**31 -1;
+    bytes_out = pickle.dumps(data);
     with open("./averages/{filename}.pickle".format(filename=fName), "wb") as f:
-        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+        for idx in range(0,n_bytes,max_bytes):
+            f.write(bytes_out[idx:idx+max_bytes]);
 def loadData(fName="graph"):
-    with open("./averages/{filename}.pickle".format(filename=fName), "rb") as f:
-        data = pickle.load(f);
+    filepath = "./averages/{filename}.pickle".format(filename=fName);
+    bytes_in = bytearray(0);
+    input_size = os.path.getsize(filepath);
+    max_bytes = 2**32 -1;
+    with open(filepath, "rb") as f:
+        for _ in range(0,input_size,max_bytes):
+            bytes_in += f.read(max_bytes);
+
+    data = pickle.loads(bytes_in);
     return data;
 
