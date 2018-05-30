2803eeeebb99e2fccca56451dbd637745c26bf16
simonsandell <simonsandell@live.se>
2018-05-30 09:42:40 +0200

    new scripts

---
+++ scripts/plotGen.py
@@ -0,0 +1,24 @@
+from plotting import datatableToPlots
+import sys
+import settings
+
+
+dirpath = settings.root_path+"modular/datatables/combined/";
+savename = "combined";
+datatableToPlots.datatableToPlots(dirpath,savename);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
