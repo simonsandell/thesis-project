34dd034d1456fcb31bf8a8f4068078c1d9ea2a46
simonsandell <simonsandell@live.se>
2018-06-01 15:50:31 +0200

    scripts to edit plot files quickly

--- shellscripts/awkprinter.sh
+++ shellscripts/awkprinter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for filename in ./*$1*.txt; do
-	head -n 2 $filename | tail -n 1 | awk -F "    " '{print $1,$30,$41,$41 / $30}';
+	awk '{print $1,$3/$5}' $filename 
 	# print system size, N_mc_avgs, error_m
 done
